e1f035604415abc018ec54ed83fb373510b390fc
arblade <raphael.arblade@gmail.com>
2024-06-03 15:35:16 +0200

    adding cli

---
+++ nyx/cli.py
@@ -0,0 +1,42 @@
+import argparse
+from pathlib import Path
+import yaml
+import sys
+
+sys.path.append(str(Path(__file__).parent.parent.resolve()))
+
+from nyx.model import NyxRule
+from nyx.exceptions import FormatException
+
+
+def parse_yaml(file_path: Path):
+    with open(file_path) as f:
+        raw = f.read()
+    return yaml.safe_load(raw)
+
+
+def check_keys(yaml_dict: dict):
+    NON_OPTIONAL_KEYS = ["title", "id", "level", "detection", "protocol"]
+    for key in NON_OPTIONAL_KEYS:
+        if key not in yaml_dict.keys():
+            raise FormatException(f"Field missing : {key}")
+
+
+def convert_from_dict(yaml_dict: dict):
+    return NyxRule.from_dict(yaml_dict)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Print the given file path.")
+    parser.add_argument("file_path", type=str, help="The path of the rules file")
+
+    args = parser.parse_args()
+    rule_file = args.file_path
+
+    rule_file = Path(__file__).parent.parent / "tests" / "rule.yml"
+    parsed = parse_yaml(rule_file)
+    check_keys(parsed)
+
+    rule = convert_from_dict(parsed)
+    print(rule.convert())
